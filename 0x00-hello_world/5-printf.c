9412b1835ec344d1e1d57d289c60cf8eccb57baa
Benfill <anasse.benfillous@gmail.com>
2023-07-13 22:59:02 +0100

    updating first c project

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 
 /**
  * main - programme prints the output of printf
- * Return:0(succes)
+ * Return:0
  **/
 
 int	main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
