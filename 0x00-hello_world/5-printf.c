f5b8a513a7c6d8c21014299b958c34ee249f7dd2
Benfill <anasse.benfillous@gmail.com>
2023-07-13 22:11:49 +0100

    updating first c project

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-int	main()
+/**
+ * main - programme prints the output of printf
+ * Return:0(succes)
+ **/
+
+int	main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,");
-	return 0;
+	return (0);
 }
