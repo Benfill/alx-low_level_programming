c1771196b1f99ea11c9d5b353c31a25c2add0f47
Benfill <anasse.benfillous@gmail.com>
2023-07-14 01:24:48 +0100

    print to the stderr

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - printing string to stderr
+ * Return: 1
+ */
+
+int	main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58, 1, stderr);
+
+	return (1);
+}
