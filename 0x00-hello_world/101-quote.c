85f63661c414e6aeccae78a32a755325e515b1aa
Benfill <anasse.benfillous@gmail.com>
2023-07-14 01:37:45 +0100

    print to the stderr

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,7 +7,7 @@
 
 int	main(void)
 {
-	write(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
 
-	return (0);
+	return (1);
 }
