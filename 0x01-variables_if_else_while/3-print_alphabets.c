7b4994103fcfd6b8bbc243fe077676c71bab9d8c
Benfill <anasse.benfillous@gmail.com>
2023-07-14 07:01:50 +0100

    printing the alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - printing the alphabet
+ * Return: 0
+ */
+
+int	main(void)
+{
+	char	alpha;
+
+	alpha = 97;
+	while (alpha <= 122)
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	alpha = 65;
+	while (alpha <= 90)
+	{
+		putchar(alpha);
+		alpha++;
+	}
+	putchar('\n');
+	return (0);
+}
