7349489b8d6ac6be3fdae0949b7cf90478e725c1
Benfill <anasse.benfillous@gmail.com>
2023-07-14 19:52:48 +0100

    printing the all possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - priting all possible combinations of single-digit 
+ * Return: 0
+ */
+
+int	main(void)
+{
+	int	digit;
+
+	digit = 48;
+	while (digit < 58)
+	{
+		putchar(digit);
+		if (digit != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		digit++;
+	}
+	putchar('\n');
+	return (0);
+}
