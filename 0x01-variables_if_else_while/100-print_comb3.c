219c05d4fb76a62659843da2b4b14728bbfdc2a6
Benfill <anasse.benfillous@gmail.com>
2023-07-15 02:21:07 +0100

    printing the combinaton of two digit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/**
+ * main - printing the combinaton of two digit
+ * Retun: 0
+ */
+
+int	main(void)
+{
+	int	digit;
+	int	digit2;
+
+	digit	= 48;
+	digit2	= 48;
+	while (digit2 != 58)
+	{
+		while (digit != '9')
+		{
+			putchar(digit);
+			putchar(digit2);
+			if (digit != '8')
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			if (digit2 == '9')
+			{
+				digit++;
+				digit2 = '1';
+			}
+			if (digit > digit2 || digit == digit2)
+			{
+				while (digit > digit2 || digit == digit2)
+					digit2++;
+			}
+			else
+				digit2++;
+		}
+		digit2++;
+
+	}
+	putchar('\n');
+	return (0);
+}
