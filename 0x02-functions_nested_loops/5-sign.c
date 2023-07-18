d1a8181fc569336eaeb09534feb51b6ca955d593
Benfill <anasse.benfillous@gmail.com>
2023-07-18 22:38:51 +0100

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_sign - check if the n greater than 0 or not
+ * @n: the number to be checked
+ * Return: 1 if n greater than 0, 0 if it is 0, -1 if n less
+ */
+int	print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else
+	{
+		putchar('-');
+		return (-1);
+	}
+}
