b5f50e0dc4eea69b02ac847e46e4b59692272ce0
Benfill <anasse.benfillous@gmail.com>
2023-07-18 21:30:03 +0100

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - check if c is lowercase
- * parameter c - is the charcter that it'll be checked
+ * parameter 'c' - is the charcter that it'll be checked
  * Return: 1 if c lowercase or 0 if its not
  */
 
