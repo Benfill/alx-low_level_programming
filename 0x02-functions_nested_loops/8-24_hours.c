5d72974bad0325676596e1046cfb8d5b70ab6ec9
Benfill <anasse.benfillous@gmail.com>
2023-07-19 01:39:01 +0100

    prints every minute of the day

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * jack_bauer -  a function that prints every minute of the day
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int	a, b, c, d;
+
+	a = 0;
+	b = 0;
+	c = 0;
+	d = 0;
+	while (a < 3)
+	{
+		while (b < 10)
+		{
+			while (c < 6)
+			{
+				while(d < 10)
+				{
+					putchar(48 + a);
+					putchar(48 + b);
+					write(1, ":", 1);
+					putchar(48 + d);
+					putchar(48 + c);
+					write(1, "\n", 1);
+					d++;
+				}
+				d = 0;
+				c++;
+			}
+			c = 0;
+			b++;
+		}
+		b = 0;
+		a++
+	}
+}
