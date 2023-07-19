5d3d5bae1c7b85798a21e7182c161cc1614b927f
Benfill <anasse.benfillous@gmail.com>
2023-07-19 03:24:40 +0100

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -6,49 +6,38 @@
  * Return: void
  */
 
-int my_putnbr(int nb)
+void print_to_98(int n)
 {
-    unsigned int a;
-    if (nb < 0)
-    {
-        _putchar('-');
-        my_putnbr(-nb);
-    }
-    if (nb >= 10)
-    {
-	a = nb % 10;
-	nb = nb / 10;
-	my_putnbr(nb);
-	_putchar(48 + a);
-    }
-    return (0);
-}
-
-void	print_to_98(int n)
-{
-	while (n < 98 || n > 98)
+	if (n <= 98)
 	{
-		if (n >= 10)
-			if (n < 100)
-				_putchar(48 + n / 10);
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
 			else
-				_putchar(48 + n / 100);
-			my_putnbr(n);
-		else
-			my_putnbr(n);
-		if (n != 98)
-			write(1, ", ", 2);
-		else
-			break;
-		if (n > 98)
-			n--;
-		else if (n < 98)
-			n++;
+			{
+				printf("%d, ", n);
+			}
+		}
 	}
-	if (n == 98)
+	else
 	{
-		_putchar(48 + n / 10);
-		_putchar(48 + n % 10);
+		for (; n >= 98; n--)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, ", n);
+			}
+		}
 	}
-	write(1, "\n", 1);
 }
