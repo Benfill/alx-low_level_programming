cb4ebd313ff3457d4faffe294726f30a2fa9a423
Benfill <anasse.benfillous@gmail.com>
2023-07-20 17:50:44 +0100

    More functions nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,14 +5,15 @@ i
  * @n: the number of times \ will be printed
  * Return: void
  */
+
 void print_diagonal(int n)
 {
-	while(n > 0)
+	while (n > 0)
 	{
 		_putchar('\n');
 		n--;
 	}
-	if(n < 0)
+	if (n < 0)
 		_putchar('\n');
 	else
 	{
