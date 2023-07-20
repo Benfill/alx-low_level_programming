fe93dc6359b2179971cd80f9932037c2e3f2214c
Benfill <anasse.benfillous@gmail.com>
2023-07-20 19:00:26 +0100

    More functions nested loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include	"main.h"
+
+/**
+ * main - FizzBuzz program
+ * Return: 0
+ */
+
+int	main(void)
+{
+	int	nbr;
+
+	nbr =	1;
+	while (nbr <= 100)
+	{
+		if (nbr % 3 == 0 && nbr % 5 == 0)
+			printf("FizzBuzz");
+		else if (nbr % 3 == 0)
+			printf("Fizz");
+		else if (nbr % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d ", nbr);
+		if (nbr != 100)
+			putchar(' ');
+		nbr++;
+	}
+	putchar('\n');
+}
