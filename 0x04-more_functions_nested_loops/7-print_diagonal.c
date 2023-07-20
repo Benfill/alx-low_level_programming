cf5571a9b41ff148854b1841039b40c1a67cb304
Benfill <anasse.benfillous@gmail.com>
2023-07-20 17:53:01 +0100

    More functions nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -21,11 +21,3 @@ void print_diagonal(int n)
 		_putchar('\n');
 	}
 }
-int main(void)
-{
-    print_diagonal(0);
-    print_diagonal(2);
-    print_diagonal(10);
-    print_diagonal(-4);
-    return (0);
-}
