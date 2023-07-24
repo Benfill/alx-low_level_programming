ec6a5f726f14cf9bb5c752a951cf4ad61a39e902
Benfill <anasse.benfillous@gmail.com>
2023-07-25 00:59:07 +0100

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -17,7 +17,7 @@ int	_atoi(char *s)
 	i =		0;
 	sum =		0;
 	sign =		1;
-	while (s[i] == ' ')
+	while (s[i] == ' ' || s[i] == '+')
 		i++;
 	while (s[i] == '-' || s[i] == '+')
 	{
@@ -32,12 +32,12 @@ int	_atoi(char *s)
 		while (s[i] >= '0' && s[i] <= '9')
 		{
 			if (sum > INT_MAX / 10
-			|| (sum == INT_MAX / 10 && s[i] - '0' > 7)) 
+			|| (sum == INT_MAX / 10 && s[i] - '0' > 7))
 			{
 				if (sign == 1)
-					return INT_MAX;
+					return (INT_MAX);
 				else
-					return INT_MIN;
+					return (INT_MIN);
 			}
 			digit = s[i] - 48;
 			sum = sum * 10 + digit;
