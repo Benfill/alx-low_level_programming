e46903a0305df8f35217fbc9a7fe8d9675afb00f
Benfill <anasse.benfillous@gmail.com>
2023-07-24 21:52:16 +0100

    pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include	"main.h"
+
+/**
+ * puts_half - print the half of a string
+ * @str: the string
+ * Return: void
+ */
+
+void	puts_half(char *str)
+{
+	int	i;
+	int	n;
+
+	i = 0;
+	while (str[i])
+		i++;
+	n = i / 2;
+	while (str[n])
+	{
+		_putchar(str[n]);
+		n++;
+	}
+	_putchar('\n');
+}
