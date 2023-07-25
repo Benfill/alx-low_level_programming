0b007165a3dbf3c4bd2524ea236bb87dae107c41
Benfill <anasse.benfillous@gmail.com>
2023-07-25 01:39:54 +0100

    pointers_arrays_strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -13,19 +13,10 @@ int	main(void)
 	int i = 0;
 	int randomizer = 0;
 	int N = 8;
-	char *numbers;
-	char *letter;
-	char *LETTER;
-	char *symbols;
-	char *password;
-	
-
-	srand((unsigned int)(time(NULL)));
-
-	numbers[10] = "0123456789";
-	letter[26] = "abcdefghijklmnoqprstuvwyzx";
-	LETTER[26] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
-	symbols[8] = "!@#$^&*?";;
+	char numbers[] = "0123456789";
+	char letter[] = "abcdefghijklmnoqprstuvwyzx";
+	char LETTER[] = "ABCDEFGHIJKLMNOQPRSTUYWVZX";
+	char symbols[] = "!@#$^&*?";
 
 	randomizer = rand() % 4;
 	for (i = 0; i < N; i++)
