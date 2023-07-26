1eda9c00ac435035219707e383dd879410ad5dc3
Benfill <anasse.benfillous@gmail.com>
2023-07-26 22:58:19 +0100

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,16 +13,16 @@ char	*_strncpy(char *dest, char *src, int n)
 	int	i;
 
 	i =	0;
+	if (dest == NULL || src == NULL)
+	    {
+		    return NULL;
+	    }
 	while (i < n && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
 	}
-	if (i < n)
-	{
-		dest[i] = '\0';
-		i++;
-	}
+
 	while (i < n)
 	{
 		dest[i] = '\0';
