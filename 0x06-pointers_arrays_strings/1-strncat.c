956edb2d1ee93d311333b5e0e1cad020e6d647a1
Benfill <anasse.benfillous@gmail.com>
2023-07-26 21:27:22 +0100

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -22,9 +22,9 @@ char	*_strncat(char *dest, char *src, int n)
 			while (i < n)
 			{
 				dest[j + i] = src[i];
+				i++;
 				if (src[i] == '\0')
 					break;
-				i++;
 			}
 			break;
 		}
@@ -32,4 +32,23 @@ char	*_strncat(char *dest, char *src, int n)
 	}
 	dest[j + i] = '\0';
 	return (dest);
+}
+
+int main(void)
+{
+    char s1[98] = "Hello ";
+    char s2[] = "World!\n";
+    char *ptr;
+
+    printf("%s\n", s1);
+    printf("%s", s2);
+    ptr = _strncat(s1, s2, 1);
+    printf("%s\n", s1);
+    printf("%s", s2);
+    printf("%s\n", ptr);
+    ptr = _strncat(s1, s2, 1024);
+    printf("%s", s1);
+    printf("%s", s2);
+    printf("%s", ptr);
+    return (0);
 }
