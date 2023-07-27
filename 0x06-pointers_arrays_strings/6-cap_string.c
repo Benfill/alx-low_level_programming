de585af978f7e4e897de2ca040777fde1840cc15
Benfill <anasse.benfillous@gmail.com>
2023-07-27 20:35:07 +0100

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,39 @@
+#include	"main.h"
+
+/**
+ * cap_string - function capitalizes all words of a string
+ * @str: string
+ * Return: pointer to the string
+ */
+
+char	*cap_string(char *str)
+{
+	int	i;
+
+	i =	0;
+	while (str[i])
+	{
+		if (str[i] == ',' || str[i] == ';' || str[i] == '.' || str[i] == ' ')
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+				str[i + 1] -= 32;
+		}
+		else if (str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '\n')
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+				str[i + 1] -= 32;
+		}
+		else if (str[i] == '(' || str[i] == ')' || str[i] == '{' || str[i] == '}')
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+				str[i + 1] -= 32;
+		}
+		else if (str[i] == '\t')
+		{
+			if (str[i + 1] >= 97 && str[i + 1] <= 122)
+				str[i + 1] -= 32;
+		}
+		i++;
+	}
+	return (str);
+}
