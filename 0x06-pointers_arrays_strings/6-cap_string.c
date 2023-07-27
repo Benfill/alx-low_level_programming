38ecad6c425f20e580dadbcb0e17ca1b70b4b5f4
Benfill <anasse.benfillous@gmail.com>
2023-07-27 20:48:17 +0100

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -18,7 +18,7 @@ char	*cap_string(char *str)
 			if (str[i + 1] >= 97 && str[i + 1] <= 122)
 				str[i + 1] -= 32;
 		}
-		else if (str[i] == '!' || str[i] == '?' || str[i] == '\"' || str[i] == '\n')
+		else if (str[i] == '!' || str[i] == '?' || str[i] == '"' || str[i] == '\n')
 		{
 			if (str[i + 1] >= 97 && str[i + 1] <= 122)
 				str[i + 1] -= 32;
@@ -33,6 +33,8 @@ char	*cap_string(char *str)
 			if (str[i + 1] >= 97 && str[i + 1] <= 122)
 				str[i + 1] -= 32;
 		}
+		else if (str[0] >= 97 && str[0] <= 122)
+			str[0] -= 32;
 		i++;
 	}
 	return (str);
