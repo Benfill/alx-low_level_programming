43abe9557b7e9574e49ba370e558ac2727afce61
Benfill <anasse.benfillous@gmail.com>
2023-07-31 21:00:23 +0100

    0x07-pointers_arrays_strings

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include	"main.h"
+
+/**
+ * _memcpy - function copies n bytes from src to dest
+ * @dest: pointer to dest memory
+ * @src: pointer to src memory
+ * @n: number of bytes to be copied
+ */
+
+char	*_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int	i;
+
+	i =	0;
+	while (i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	return (dest);
+}
