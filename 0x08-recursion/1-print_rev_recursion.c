cd77f0c6b6f343778139adeebacd7d3d1e989a3b
Benfill <anasse.benfillous@gmail.com>
2023-08-02 21:03:27 +0100

    0x08-recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,20 @@
+#include	"main.h"
+
+/**
+ * _print_rev_recursion - function prints a string in reverse
+ * @s: pointer to the string
+ * Return: void
+ */
+
+void	_print_rev_recursion(char *s)
+{
+	char	c;
+
+	if (*s != '\0')
+	{
+		c = *s;
+		s++;
+		_print_rev_recursion(s);
+	}
+	putchar(c);
+}
