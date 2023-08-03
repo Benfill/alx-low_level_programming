6533f5140ed33faf7152f318ae10eac264f942d9
Benfill <anasse.benfillous@gmail.com>
2023-08-03 21:42:22 +0100

    0x08-recursion

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,46 +1,30 @@
 #include	"main.h"
 
 /**
- * is_prime_number - function that returns 1 if the input integer is a prime number
+ * actual_prime - calculates if a number is prime recursively
+ * @n: number to evaluate
+ * @i: iterator
+ * Return: 1 if n is prime, 0 if not
+ */
+
+int	actual_prime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (actual_prime(n, i - 1));
+}
+
+/**
+ * is_prime_number - function that returns 1 if the input integer is a prime
  * @n: the number
  * Return: 1 if the integer is a prime number, otherwise return 0
  */
 
 int	is_prime_number(int n)
 {
-	if (n == 1 || n == 0)
-	{
+	if (n <= 1)
 		return (0);
-	}
-	else if (n % 2 == 0 || n % 3 == 0)
-	{
-		return (0);
-	}
-	else
-	{
-         is_prime_number(n1);
-	}
-}
-
-int main(void)
-{
-    int r;
-
-    r = is_prime_number(1);
-    printf("%d\n", r);
-    r = is_prime_number(1024);
-    printf("%d\n", r);
-    r = is_prime_number(16);
-    printf("%d\n", r);
-    r = is_prime_number(17);
-    printf("%d\n", r);
-    r = is_prime_number(25);
-    printf("%d\n", r);
-    r = is_prime_number(-1);
-    printf("%d\n", r);
-    r = is_prime_number(113);
-    printf("%d\n", r);
-    r = is_prime_number(7919);
-    printf("%d\n", r);
-    return (0);
+	return (actual_prime(n, n - 1));
 }
