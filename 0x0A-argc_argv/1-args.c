c9b29745b0fb4c1ad82ae0d91f65fee5d3995f97
Benfill <anasse.benfillous@gmail.com>
2023-08-05 00:05:08 +0100

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include	"main.h"
+
+/**
+ * main - program prints the number of args passed into it
+ * @argc: number of args
+ * @argv: array of argument
+ * Return: 0
+ */
+
+int	main(int argc, char **argv)
+{
+	if (argc >= 0)
+		putchar(47 + argc);
+	return (0);
+}
