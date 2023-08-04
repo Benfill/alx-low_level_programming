2a0fe6f243856b645d5b26a48f5bba178f12f600
Benfill <anasse.benfillous@gmail.com>
2023-08-04 02:25:31 +0100

    0x08-recursion

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,41 +1,33 @@
 #include	"main.h"
 
 /**
- * palCheck - Helper function to check if a string is a palindrome recursively
+ * _strlen_recursion - function returns the length of a string
+ * @s: a pointer to the string
+ * Return: the length
+ */
+
+int	_strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
+
+/**
+ * is_palindrome_recursive - function to check if a string is a palindrom
  * @s: A pointer to the original string to check for palindrome
- * @str: A buffer to store a copy of the string (used for comparison)
- * @on: A flag to indicate whether to copy the string (1) or compare (0)
- * @i: The index to traverse the original string in 'on' state
- * @j: The index to traverse the copied string in 'off' state
+ * @start: the start of s
+ * @end: the end of s
  * Return: 1 if string is a palindrome and 0 if not
  */
 
-int	palCheck(char *s, char *str, int on, int i, int j)
+int	is_palindrome_recursive(char *s, int start, int end)
 {
-	if (on == 1)
-	{
-		if (s[i] == '\0')
-		{
-			str[i] = '\0';
-			on = 0;
-		}
-		else
-		{
-			str[i] = s[i];
-			return (palCheck(s, str, on, i + 1, j));
-		}
-	}
-	if (on == 0)
-	{
-		if (str[j] != '\0')
-		{
-			if (s[i - 1] != str[j])
-				return (0);
-			else
-				return (palCheck(s, str, on, i - 1, j + 1));
-		}
-	}
-	return (1);
+	if (start >= end)
+		return (1);
+	if (s[start] != s[end])
+		return (0);
+	return (is_palindrome_recursive(s, start + 1, end - 1));
 }
 
 /**
@@ -44,11 +36,11 @@ int	palCheck(char *s, char *str, int on, int i, int j)
  * Return: 1 if s is palindrome and 0 if not
  */
 
-int	is_palindrome(char *s)
+int is_palindrome(char *s)
 {
-	char	str[200];
-
 	if (s == NULL)
-		return (1);
-	return (palCheck(s, str, 1, 0, 0));
+		return (0);
+	int len = _strlen_recursion(s);
+
+	return (is_palindrome_recursive(s, 0, len - 1));
 }
