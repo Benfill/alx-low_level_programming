56311854283f8d3ce4a0e42d29c1c529657a4d03
Benfill <anasse.benfillous@gmail.com>
2023-08-05 00:48:23 +0100

    0x0A-argc_argv

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,25 +1,6 @@
 #include	"main.h"
 
 /**
- * _puts - function that prints a string
- * @str: the string
- * Return: void
- */
-
-void	_puts(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		putchar(str[i]);
-		i++;
-	}
-	putchar('\n');
-}
-
-/**
  * main - program prints all args it receives
  * @argc: number of args
  * @argv: arguments
@@ -35,7 +16,7 @@ int	main(int argc, char **argv)
 	j =	0;
 	while (i < argc)
 	{
-		_puts(argv[i]);
+		puts(argv[i]);
 		i++;
 	}
 	return (0);
