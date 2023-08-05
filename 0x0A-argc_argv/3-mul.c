a9649ee6530921ae34ef0bc08f5afa4fc02b005d
Benfill <anasse.benfillous@gmail.com>
2023-08-05 01:25:02 +0100

    0x0A-argc_argv

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,7 +6,7 @@
  * Return: void
  */
 
-void	print_number(int n)
+void	print_number(long n)
 {
 	if (n < 0)
 	{
@@ -23,6 +23,46 @@ void	print_number(int n)
 }
 
 /**
+ * _atoi - function converts ascii to integer
+ * @s: the pointer of string
+ * Return: the converted integer
+ */
+
+int	_atoi(char *s)
+{
+	int	i;
+	int	sum;
+	int	digit;
+	int	sign;
+
+	i =		0;
+	sum =		0;
+	sign =		1;
+	while (s[i] == '-' || s[i] == '+' || s[i] == ' ')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
+	while (s[i])
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			sign = 1;
+		while (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - 48;
+			sum = sum * 10 + digit;
+			i++;
+		}
+		if (sum > 0)
+			break;
+		i++;
+	}
+	return (sum * sign);
+}
+
+
+/**
  * main - program multiplies two numbers
  * @argc: the number of args
  * @argv: array of arguments
@@ -33,15 +73,15 @@ int	main(int argc, char **argv)
 {
 	int	nbr1;
 	int	nbr2;
-	int	res = 0;
+	long	res = 0;
 
 	if (argc != 3)
 	{
 		write(1, "Error\n", 6);
 		return (1);
 	}
-	nbr1 = *argv[1] - 48;
-	nbr2 = *argv[2] - 48;
+	nbr1 = _atoi(argv[1]);
+	nbr2 = _atoi(argv[2]);
 	res = nbr1 * nbr2;
 	print_number(res);
 	putchar('\n');
