69984a82e646c9a1f62651ab2de15ceedff94353
Benfill <anasse.benfillous@gmail.com>
2023-08-07 05:23:35 +0100

    0x0B-malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -9,8 +9,8 @@
 
 char	*create_array(unsigned int size, char c)
 {
-	int	i;
-	char	*a;
+	unsigned int	i;
+	char		*a;
 
 	i = 0;
 	a = (char *) malloc(1 * size + 1);
@@ -25,45 +25,4 @@ char	*create_array(unsigned int size, char c)
 	}
 	a[size] = '\0';
 	return (a);
-}
-
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code for ALX School students.
- *
- * Return: Always 0.
- */
-int main(void)
-{
-    char *buffer;
-
-    buffer = create_array(98, 'H');
-    if  (buffer == NULL)
-    {
-        printf("failed to allocate memory\n");
-        return (1);
-    }
-    simple_print_buffer(buffer, 98);
-    free(buffer);
-    return (0);
 }
