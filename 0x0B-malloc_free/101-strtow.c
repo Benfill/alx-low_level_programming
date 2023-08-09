068d47aeb459d49c84d8e6b935c07a05372333a7
Benfill <anasse.benfillous@gmail.com>
2023-08-09 22:25:53 +0100

    0x0B-malloc_free

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -82,7 +82,9 @@ char	**strtow(char *str)
 
 	i =		0;
 	j =		0;
-	if ((str[0] == ' ' && str[1] == '\0') || str[0] == '\0')
+	if (str[0] == ' ' && str[1] == '\0')
+		return (NULL);
+	if (str[0] == '\0')
 		return (NULL);
 	counter =	_wordCounter(str);
 	p = (char **) malloc(sizeof(char *) * (counter + 1));
