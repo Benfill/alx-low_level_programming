ef16d92b2e8e5cc210efe1393d58332df3395f21
Benfill <anasse.benfillous@gmail.com>
2023-08-10 21:19:11 +0100

    0x0C-more_malloc_free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include	"main.h"
+
+/**
+ * malloc_checked - function that allocates memory using malloc
+ * @b: the number of bytes to be allocated
+ * Return: void
+ */
+
+void	*malloc_checked(unsigned int b)
+{
+	void	*p;
+
+	p = malloc(b);
+	if (p == NULL)
+		exit(1);
+	else
+		return (p);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+
+void _putchar(char c);
+void *malloc_checked(unsigned int b);
+#endif /* MAIN_H */
