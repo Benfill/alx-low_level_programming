f2d24314076741cc485a65fd41b209790a323abe
Benfill <anasse.benfillous@gmail.com>
2023-08-15 00:16:42 +0100

    0x0E-structures_typedef

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,6 +1,24 @@
 #include	"dog.h"
 
 /**
+ * _strlen - function that counts the lenght of a string
+ * @s: the string
+ * Return: the length of s
+ */
+
+int	_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
  * print_dog - function prints dog's infos
  * @d: pointer to struct dog
  * Return: void
@@ -8,7 +26,7 @@
 
 void	print_dog(struct dog *d)
 {
-	unsigned int	i = strlen(d->owner);
+	int	i = _strlen(d->owner);
 
 	if (d == NULL)
 		return;
