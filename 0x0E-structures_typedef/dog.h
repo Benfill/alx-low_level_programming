616d60203f48812c781f208d5df15fc49e0bfbe7
Benfill <anasse.benfillous@gmail.com>
2023-08-14 22:07:42 +0100

    0x0E-structures_typedef

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define	DOG
+
+/**
+ * struct dog - dog infos
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct	dog {
+	char	*name;
+	float	age;
+	char	*owner;
+};
+
+#endif
