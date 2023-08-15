64db02027fb9293bc2723ca7a3d380ffcbd02a08
Benfill <anasse.benfillous@gmail.com>
2023-08-16 00:22:50 +0100

    0x0D-preprocessor

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,8 +7,6 @@
 
 void	print_dog(struct dog *d)
 {
-	char	*own = (d->owner);
-
 	if (d == NULL)
 		return;
 	if (d->name == NULL)
@@ -19,7 +17,7 @@ void	print_dog(struct dog *d)
 		write(1, "Age: (nil)\n", 11);
 	else
 		printf("Age: %.6f\n", d->age);
-	if (*own == '\0' && *(own + 1) == '\0')
+	if (d->owner == (void *)0)
 		write(1, "Owner: (nil)\n", 13);
 	else
 		printf("Owner: %s\n", d->owner);
