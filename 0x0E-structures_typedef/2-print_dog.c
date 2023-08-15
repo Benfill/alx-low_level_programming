ff3861eb6c2e7519406f653aa924906813100929
Benfill <anasse.benfillous@gmail.com>
2023-08-16 00:49:32 +0100

    0x0D-preprocessor

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -15,7 +15,7 @@ void	print_dog(struct dog *d)
 		printf("Name: (nil)\n");
 	else
 		printf("Name: %s\n", d->name);
-	if (d->age == 0)
+	if (!d->age)
 		printf("Age: (nil)\n");
 	else
 		printf("Age: %.6f\n", d->age);
