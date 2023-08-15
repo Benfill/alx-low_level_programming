2276b1ba248c1c255cd2199167840cf48270f561
Benfill <anasse.benfillous@gmail.com>
2023-08-15 23:40:13 +0100

    0x0D-preprocessor

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,7 +10,10 @@
 
 void	init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	d = malloc(sizeof(struct dog));
+	if (d == NULL)
+		return;
 	d->name = name;
-	d->age = (float)age;
+	d->age = age;
 	d->owner = owner;
 }
