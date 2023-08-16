925f9b42db58933c21ffb6feb377a3e3ba40f3c9
Benfill <anasse.benfillous@gmail.com>
2023-08-16 06:35:09 +0100

    0x0F-function_pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include	"function_pointers.h"
+
+/**
+ * print_name - function prints name through pointer to other function
+ * @name: the name to be printed
+ * @f: pointer to a function
+ */
+
+void	print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef	PTR
+#define	PTR
+
+#include	<stdio.h>
+#include	<stdlib.h>
+#include	<unistd.h>
+
+void	_putchar(char c);
+void print_name(char *name, void (*f)(char *));
+
+#endif
