ebf8ca96cd8dde87192bc3fedc27c8774ef3b142
Benfill <anasse.benfillous@gmail.com>
2023-08-16 20:01:59 +0100

    0x0F-function_pointers

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,8 +13,8 @@ int	int_index(int *array, int size, int (*cmp)(int))
 {
 	int	i;
 
-	if (!cmp || !array)
-		return;
+	if (!cmp || array == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 	{
 		if (cmp(array[i]))
