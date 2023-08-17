1792be6a3441dff5f44251c861f480f899795125
Benfill <anasse.benfillous@gmail.com>
2023-08-17 23:32:46 +0100

    0x10-variadic_functions

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -16,33 +16,36 @@ void	print_all(const char * const format, ...)
 	va_list	p;
 
 	va_start(p, format);
-	while (format[i])
+	if (format)
 	{
-		switch (format[i])
+		while (format[i])
 		{
-			case 'i':
-			integer	= va_arg(p, int);
-			printf("%i", integer);
-			break;
-			case 'c':
-			c = va_arg(p, int);
-			printf("%c", c);
-			break;
-			case 'f':
-			f = va_arg(p, double);
-			printf("%f", f);
-			break;
-			case 's':
-			str = va_arg(p, char *);
-			if (!str)
-				str = "(nil)";
-			printf("%s", str);
-			break;
-			default:
+			switch (format[i])
+			{
+				case 'i':
+				integer	= va_arg(p, int);
+				printf("%i", integer);
+				break;
+				case 'c':
+				c = va_arg(p, int);
+				printf("%c", c);
+				break;
+				case 'f':
+				f = va_arg(p, double);
+				printf("%f", f);
+				break;
+				case 's':
+				str = va_arg(p, char *);
+				if (!str)
+					str = "(nil)";
+				printf("%s", str);
+				break;
+				default:
+				i++;
+				continue;
+			}
 			i++;
-			continue;
 		}
-		i++;
 	}
 	putchar('\n');
 }
