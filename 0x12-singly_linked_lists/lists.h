deebb07dde1e99242424b8e7ac802a509a704a70
Benfill <anasse.benfillous@gmail.com>
2023-08-25 01:54:19 +0100

    updating first task

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef     S_LIST
-#define     S_LIST
+#ifndef	S_LIST
+#define S_LIST
 
 #include <stdlib.h>
 #include <string.h>
@@ -16,9 +16,9 @@
 
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 void	_putchar(char c);
 size_t print_list(const list_t *h);
