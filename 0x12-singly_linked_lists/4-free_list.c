72ff52b4d62fd08cde4b4c621d2104380837baec
Benfill <anasse.benfillous@gmail.com>
2023-08-25 03:46:37 +0100

    add the 5th task

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -7,9 +7,12 @@
 
 void free_list(list_t *head)
 {
-	if (head == NULL)
-		return;
-	free_list(head->next);
-	free(head->next);
-	free(head);
+	list_t *current;
+
+	while ((current = head) != NULL)
+	{
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
 }
