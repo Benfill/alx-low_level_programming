b49efdf38af9485fea710d59c298d4b147f16e4f
Benfill <anasse.benfillous@gmail.com>
2023-08-25 06:07:46 +0100

    100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include	<stdio.h>
+
+/**
+ * first -  function that prints a string before the main function is executed
+ */
+void	first(void)
+	__attribute__ ((constructor));
+
+void	first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
