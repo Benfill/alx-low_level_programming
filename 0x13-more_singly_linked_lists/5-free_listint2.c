ced13cfb52795f6de51630672de31e575756da83
Benfill <anasse.benfillous@gmail.com>
2023-08-29 06:11:24 +0100

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include	"lists.h"
+
+/**
+ * free_listint2 - function that frees a listint_t list
+ * @head: the address of the ponter to the head
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t	*tmp;
+
+	while (*head != NULL)
+	{
+		tmp = (*head)->next;
+		free(*head);
+		*head = tmp;
+	}
+	head = NULL;
+}
