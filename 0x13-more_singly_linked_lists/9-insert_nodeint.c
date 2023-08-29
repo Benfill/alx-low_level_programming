bd7a7b97d1ac5b1473a9972b2a919f88bf03e18e
Benfill <anasse.benfillous@gmail.com>
2023-08-29 09:42:43 +0100

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -24,7 +24,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (*head != NULL)
 	{
 		tmp = *head;
-		while (tmp != NULL || count <= idx)
+		while (tmp != NULL)
 		{
 			if (count == idx)
 			{
