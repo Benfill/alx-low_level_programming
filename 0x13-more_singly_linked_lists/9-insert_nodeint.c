f53f2db92cb5c8fa849dbb4b3872cd336dc8f36c
Benfill <anasse.benfillous@gmail.com>
2023-08-29 09:28:18 +0100

    0x13-more_singly_linked_lists

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -43,9 +43,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		new->next = NULL;
 	}
 	if (count != idx)
-	{
-		free(new);
 		return (NULL);
-	}
 	return (new);
 }
