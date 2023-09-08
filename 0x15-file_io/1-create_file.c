f13f11a54bd4f68f49ad949cb5d96b15bac68a92
Benfill <anasse.benfillous@gmail.com>
2023-09-08 13:47:19 +0100

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,13 +8,10 @@
 
 int	_strlen(char *s)
 {
-	int	i;
+	int	i = 0;
 
-	i = 0;
 	while (s[i])
-	{
 		i++;
-	}
 	return (i);
 }
 
@@ -27,20 +24,20 @@ int	_strlen(char *s)
 
 int create_file(const char *filename, char *text_content)
 {
-	int	fd, fw;
+	int	fd, fw = 0;
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR | O_TRUNC);
-	if (fd == -1)
-		fd = open(filename, O_RDWR | O_CREAT);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (fd == -1)
 		return (-1);
-	if (!text_content)
-		return (1);
-	fw = write(fd, text_content, _strlen(text_content));
-	if (fw == -1)
-		return (-1);
+	if (text_content)
+		fw = write(fd, text_content, _strlen(text_content));
+		if (fw == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	close(fd);
 	return (1);
 }
