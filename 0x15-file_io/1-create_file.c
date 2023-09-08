71b5c64414963c96436dfe970e39d0c120d00cac
Benfill <anasse.benfillous@gmail.com>
2023-09-08 13:24:54 +0100

    0x15-file_io

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include	"main.h"
+
+/**
+ * create_file - function creates a file
+ * @filename: function that creates a file
+ * @text_content: is a NULL terminated string to write to the file
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int	fd, fw;
+
+	if (!filename)
+		return (-1);
+	fd = open(filename, O_RDWR | O_TRUNC);
+	if (fd == -1)
+		fd = open(filename, O_RDWR | O_CREAT);
+	if (fd == -1)
+		return (-1);
+	if (!text_content)
+		return (1);
+	fw = write(fd, text_content, strlen(text_content));
+	if (fw == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,8 +4,9 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
-
+#include <strings.h>
 void	_putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
+int create_file(const char *filename, char *text_content);
 
 #endif
