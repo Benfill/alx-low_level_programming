b6ccb69d7288fbb60799278356824f2214ae2283
Benfill <anasse.benfillous@gmail.com>
2023-09-08 13:28:30 +0100

    0x15-file_io

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <strings.h>
+
 void	_putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
