0e93dd002ad11dcc51e30a3b54dfdb6ee764e73e
Benfill <anasse.benfillous@gmail.com>
2023-09-08 13:44:06 +0100

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,6 +1,24 @@
 #include	"main.h"
 
 /**
+ * _strlen - function that counts the lenght of a string
+ * @s: the string
+ * Return: the length of s
+ */
+
+int	_strlen(char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
  * create_file - function creates a file
  * @filename: function that creates a file
  * @text_content: is a NULL terminated string to write to the file
@@ -13,14 +31,14 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR);
+	fd = open(filename, O_RDWR | O_TRUNC);
 	if (fd == -1)
 		fd = open(filename, O_RDWR | O_CREAT);
 	if (fd == -1)
 		return (-1);
 	if (!text_content)
 		return (1);
-	fw = write(fd, text_content, strlen(text_content));
+	fw = write(fd, text_content, _strlen(text_content));
 	if (fw == -1)
 		return (-1);
 	close(fd);
