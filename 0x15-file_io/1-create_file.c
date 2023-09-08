a2de2b5a45379167d8aef256634df8ea431ac453
Benfill <anasse.benfillous@gmail.com>
2023-09-08 13:47:48 +0100

    0x15-file_io

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -28,7 +28,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC);
 	if (fd == -1)
 		return (-1);
 	if (text_content)
