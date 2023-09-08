120bb105c12173a38d6c5d8b04fd3173845ebaf2
Benfill <anasse.benfillous@gmail.com>
2023-09-08 14:10:03 +0100

    0x15-file_io

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,9 +1,24 @@
 #include	"main.h"
 
 /**
+ * _strlen - function that counts the lenght of a string
+ * @s: the string
+ * Return: the length of s
+ */
+
+int	_strlen(char *s)
+{
+	int	i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
  * append_text_to_file - function appends text at the end of a file
  * @filename: is the name of the file
- * text_content: is the NULL terminated string to add at the end of the file
+ * @text_content: is the NULL terminated string to add at the end of the file
  * Return: 1 on success and -1 on failure
  */
 
@@ -16,7 +31,7 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (-1);
 	if (!text_content)
 		return (1);
-	fw = write(fd, text_content, strlen(text_content));
+	fw = write(fd, text_content, _strlen(text_content));
 	if (fw == -1)
 		return (-1);
 	return (1);
