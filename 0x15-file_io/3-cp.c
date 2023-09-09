ce8842ef3cedf909103340a44930fa337b09ec25
Benfill <anasse.benfillous@gmail.com>
2023-09-09 03:46:20 +0100

    0x15-file_io

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,12 +1,30 @@
 #include	"main.h"
 
 /**
+ * _strlen - function that counts the lenght of a string
+ * @s: the string
+ * Return: the length of s
+ */
+
+int	_strlen(char *s)
+{
+	int	i = 0;
+
+	while (s[i])
+		i++;
+	return (i);
+}
+
+/**
  * main - program that copies the content of a file to another file
+ * @ac: arg count
+ * @av: args
+ * Return: 0
  */
 
 int	main(int ac, char **av)
 {
-	char *buff[1024], *file_to, *file_from;
+	char buff[1024], *file_to, *file_from;
 	int	fd_from, fd_to;
 
 	if (ac != 3)
@@ -14,7 +32,11 @@ int	main(int ac, char **av)
 		write(STDERR_FILENO, "Usage: cp file_from file_to\n", 29);
 		exit(97);
 	}
-	file_from = *av[1];
-	file_to = *av[2];
-	fd_to = open();
+	file_from = av[1];
+	file_to = av[2];
+	fd_from = open(file_from, O_RDWR);
+	read(fd_from, buff, 1024);
+	buff[_strlen(buff) + 1] = '\0';
+	fd_to = open(file_to, O_RDWR);
+	return (0);
 }
