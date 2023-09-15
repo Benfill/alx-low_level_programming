78a062dfa700e6e80e22d2d1de954a8bd4a25705
Benfill <anasse.benfillous@gmail.com>
2023-09-15 17:25:55 +0100

    adding the third exercice

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,51 @@
 #include	"main.h"
 
 /**
+ * create_buff - function allocates a space for buffer
+ * @file_to: pointer to the name of file to
+ * Return: pointer to ther buffer
+ */
+char	*create_buff(char *file_to)
+{
+	char	*buff;
+
+	buff = malloc(1024);
+	if (!buff)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+	return (buff);
+}
+
+/**
+ * close_fd - function closes the fds
+ * @buff: pointer to the buffer
+ * @fd_to: to file descriptor
+ * @fd_from: from file discriptor
+ */
+
+void	close_fd(char *buff, int fd_to, int fd_from)
+{
+	int	tmp;
+
+	tmp = close(fd_from);
+	if (tmp == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		free(buff);
+		exit(100);
+	}
+	tmp = close(fd_to);
+	if (tmp == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		free(buff);
+		exit(100);
+	}
+}
+
+/**
  * _strlen - function counts chars from string
  * @str: the string to be counted
  * Return: the size of the string
@@ -24,20 +69,25 @@ int	_strlen(char  *str)
 
 int	main(int ac, char **av)
 {
-	char buff[1024], *file_to, *file_from;
+	char *buff, *file_to, *file_from;
 	int	fd_from, fd_to, tmp, byte_readed;
+
 
 	if (ac != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		free(buff);
 		exit(97);
 	}
 	file_from = av[1];
 	file_to = av[2];
 	fd_from = open(file_from, O_RDWR);
+	buff = create_buff(file_to);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		free(buff);
+		close(fd_from);
 		exit(98);
 	}
 	byte_readed = read(fd_from, buff, 1024);
@@ -47,19 +97,11 @@ int	main(int ac, char **av)
 	if (tmp == -1 || fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		free(buff);
+		close_fd(buff, fd_to, fd_from);
 		exit(99);
 	}
-	tmp = close(fd_from);
-	if (tmp == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	tmp = close(fd_to);
-	if (tmp == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+	close_fd(buff, fd_to, fd_from);
+	free(buff);
 	return (0);
 }
