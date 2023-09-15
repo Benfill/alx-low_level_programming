b0f5cc902516e05f8b1659f0f6d97f4e41d1aacf
Benfill <anasse.benfillous@gmail.com>
2023-09-15 10:13:31 +0100

    adding the third exercice

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,21 @@
 #include	"main.h"
 
 /**
+ * _strlen - function counts chars from string
+ * @str: the string to be counted
+ * Return: the size of the string
+ */
+
+int	_strlen(char  *str)
+{
+	int	i = 0;
+
+	while (str[i])
+		i++;
+	return (i);
+}
+
+/**
  * main - program that copies the content of a file to another file
  * @ac: arg count
  * @av: args
@@ -10,16 +25,46 @@
 int	main(int ac, char **av)
 {
 	char buff[1024], *file_to, *file_from;
-	int	fd_from, fd_to;
+	int	fd_from, fd_to, tmp;
 
 	if (ac != 3)
 	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file_from = av[1];
 	file_to = av[2];
 	fd_from = open(file_from, O_RDWR);
+	if (fd_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+		exit(98);
+	}
 	read(fd_from, buff, 1024);
-	fd_to = open(file_to, O_RDWR);
+	buff[_strlen(buff) + 1] = '\0';
+	fd_to = open(file_to, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+	tmp = write(fd_to, buff, _strlen(buff));
+	if (tmp == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+		exit(99);
+	}
+	tmp = close(fd_from);
+	if (tmp == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		exit(100);
+	}
+	tmp = close(fd_to);
+	if (tmp == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		exit(100);
+	}
 	return (0);
 }
